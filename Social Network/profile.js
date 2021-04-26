ad29907f5ddcbbf93ba453eee6d1052e3f1cb8ed
branavann <76181597+branavann@users.noreply.github.com>
2021-04-26 10:56:18 -0400

    Create profile.js

---
+++ Social Network/profile.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { fetchUserData, cancelFetch } from './dataFetcher';
+import { Userlist } from './Userlist';
+
+export class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { userData: null };
+  }  
+  
+  loadUserData() {
+    this.setState({ userData: null});
+    this.fetchID = fetchUserData(this.props.username, (userData) => {
+      this.setState({ userData });
+    });
+  }
+
+  render() {
+
+    let isLoading;
+    let name;
+    let bio;
+    let friends;
+
+    if (this.state.userData === null) {
+      isLoading = true;
+      name = 'Loading...';
+      bio = 'I love to play and have fun :)';
+      friends = [];
+    } else {
+      isLoading = false;
+      name = this.state.userData.name;
+      bio = this.state.userData.bio;
+      friends = this.state.userData.friends;
+    }
+
+
+    let className = 'Profile';
+    if (isLoading) {
+      className += ' loading';
+    }
+
+    return (
+      <div className={className}>
+        <div className="profile-picture">
+          { !isLoading && <img src={this.state.userData.profilePictureUrl} alt="" />}
+        </div>
+        <div className="profile-body">
+          <h2>{name}</h2>
+          <h3>@{this.props.username}</h3>
+          <p>{bio}</p>
+          <h3>My friends</h3>
+          <Userlist usernames={friends} onChoose={this.props.onChoose} />
+        </div>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.loadUserData();
+  }
+
+  componentWillUnmount() {
+    cancelFetch(this.fetchID);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.username !== prevProps.username) {
+      cancelFetch(this.fetchID);
+      this.loadUserData();
+    }
+  }
+}
